535218ca4284382a4208c5621d03419cf8f56fc2
Baxter <baxter.berdinner@gmail.com>
2025-02-18 12:14:37 +1300

    renaming, formatting

--- demo/hello_world/index.tsx
+++ demo/hello_world/index.tsx
@@ -10,7 +10,10 @@ const highlight = "#ff00aa" as any;
 const pixel = 4 as any;
 
 const styles = {
-  container: { // Main container for all elements.
+
+  // Containers
+
+  mainContainer: { // Main container for all elements.
     'width': 320,
     'height': 240,
     'background-color': offWhite,
@@ -20,7 +23,7 @@ const styles = {
     'border-width': 0,
     'display': 'flex',
   },
-  section1: { // Black section for titles etc.
+  blackBox: { // Black section for titles etc.
     'width': '100%',
     'height': 'auto',
     'background-color': black,
@@ -30,8 +33,8 @@ const styles = {
     'display': 'flex',
     'flex-direction': 'column',
   },
-  section2: { // Gray section, fits inside section1.
-    'width': '48.75%',
+  whiteBox: { // White section, fits inside blackBox.
+    'width': '48%',
     'height': 'auto',
     'background-color': offWhite,
     'border-radius': 0,
@@ -40,6 +43,9 @@ const styles = {
     'display': 'flex', 
     'flex-direction': 'column', 
   },
+
+  // Text
+
   title: { // Large title text.
     'font-size': 32,
     'text-color': offWhite,
@@ -59,6 +65,9 @@ const styles = {
     'width': 310,
     'text-wrap': 1,
   },
+
+  // Buttons
+
   button: { // Red button with square corners.
     'background-color': black,
     'border-radius': 0, 
@@ -81,6 +90,9 @@ const styles = {
   buttonText: {
     'text-color': offWhite,
   },
+
+  // Checkboxes
+
   indicator: {
     'border-color': black,
     'background-color': offWhite,
@@ -96,7 +108,14 @@ const styles = {
   indicatorText: {
     'text-color': offWhite,
   },
-  line: { // Thin red line to seperate sections.
+
+  // Lines
+
+  whiteLine: { // Thin white line to seperate sections.
+    'line-color': offWhite,
+    'line-width': pixel
+  },
+  blackLine: { // Thin black line to seperate sections.
     'line-color': black,
     'line-width': pixel
   }
@@ -105,34 +124,34 @@ const styles = {
 function App() {
 
   return (
-    <View style={styles.container}>
-      <View style={styles.section1}>
+    <View style={styles.mainContainer}>
+      <View style={styles.blackBox}>
         <Text style={styles.title}>
           UI STYLING TEST
         </Text>
       </View>
-      <Line style={styles.line}  points={[[0,0],[320,0]]}/>
+      <Line style={styles.blackLine}  points={[[0,0],[320,0]]}/>
       <Text style={[styles.small, { 'padding-left': pixel }]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
-      <Line style={styles.line}  points={[[0,0],[320,0]]}/>
-      <View style={[styles.section1, { 'flex-direction': 'row' }]}>
-        <View style={styles.section2}>
+      <Line style={styles.blackLine}  points={[[0,0],[320,0]]}/>
+      <View style={[styles.blackBox, { 'flex-direction': 'row' }]}>
+        <View style={styles.whiteBox}>
           <Text style={[styles.body, { 'text-align': 'justify', 'width': '100%' }]}>
             Text for the left-hand side!
           </Text>
         </View>
-        <View style={styles.section2}>
+        <View style={styles.whiteBox}>
           <Text style={[styles.body, { 'text-align': 'justify', 'width': '100%' }]}>
             Text for the right-hand side!
           </Text>
         </View>
       </View>
-      <Line style={styles.line}  points={[[0,0],[320,0]]}/>
+      <Line style={styles.blackLine}  points={[[0,0],[320,0]]}/>
       <Text style={[styles.body, { 'padding-left': pixel }]}>The quick brown fox jumps over the lazy dog, to which the dog barks and the fox runs away. The fox runs to the forest and the dog follows. The dog is a good dog and the fox is a good fox.</Text>
       <Button style={styles.button} onPressedStyle={styles.buttonPressed}>
         <Text style={styles.buttonText}>PRESS ME</Text>
       </Button>
 
-      <View style={styles.section1}>
+      <View style={styles.blackBox}>
             <Checkbox
                 checked={false}
                 text="Apple"
@@ -154,9 +173,9 @@ function App() {
             />
         </View>
 
-      <Line style={styles.line}  points={[[0,0],[320,0]]}/>
+      <Line style={styles.blackLine}  points={[[0,0],[320,0]]}/>
       <Text style={styles.body}>She sells sea shells by the sea shore. The shells that she sells are sea shells I'm sure. So if she sells sea shells by the sea shore, I'm sure that the shells are sea shore shells.</Text>
-      <Line style={styles.line}  points={[[0,0],[320,0]]}/>
+      <Line style={styles.blackLine}  points={[[0,0],[320,0]]}/>
     </View>
   );
 }
