38dbfd95abd46844e9552e95f5754bda1c9ea217
Baxter <baxter.berdinner@gmail.com>
2025-02-17 16:58:14 +1300

    start making style sheet

--- demo/hello_world/index.tsx
+++ demo/hello_world/index.tsx
@@ -1,50 +1,87 @@
-import { Button, Line, Render, Text, View } from "lvgljs-ui";
+import { Button, EAlignType, Line, Render, Text, Textarea, View } from "lvgljs-ui";
 import React, { useEffect, useState } from "react";
 
 const URL = "https://mastodon.social/api/v1/timelines/public";
 
-const dark = "#303030";
-const light = "#f3f3f3";
+const dark = "#000000" as any;
+const gray = '#92938d' as any;
+const light = "#e8d6c0" as any;
+const highlight = "#a1281c" as any;
+
+const styles = {
+  container: {
+    'width': 320,
+    'height': 240,
+    'background-color': light,
+    'flex-direction': 'column',
+    'padding': 2,
+    'gap': 10,
+    'display': 'flex',
+    'align-items': 'flex-start',
+  },
+  section: {
+    'width': '100%',
+    'height': 'auto',
+    'background-color': gray,
+    'border-radius': 0,
+    'border-width': 0,
+    'padding': 2,
+  },
+  title: {
+    'font-size': 32,
+    'text-color': dark,
+    'border-radius': 5,
+    'border-width': 2,
+    'border-color': gray,
+    'background-color': highlight,
+  },
+  body: {
+    'font-size': 16,
+    'text-color': dark,
+  },
+  small: {
+    'font-size': 12,
+    'text-color': gray,
+  },
+  button: {
+    'background-color': highlight,
+    'border-radius': 0,
+    'border-width': 0,
+  }
+};
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) =>
-        setData(data.map((post) => post.content.replace(/<[^>]*>?/gm, ""))),
-      )
-      .catch((error) => console.error("Error:", error));
-  }, []);
 
   return (
-    <View
-      style={{
-        width: 320,
-        height: 240,
-        "background-color": light,
-        display: "flex",
-        "flex-direction": "column",
-      }}
-    >
-      <Text style={{ "font-size": 24, "text-color": dark }}>
-        {data.length} posts
-      </Text>
-      {data.map((post) => (
-        <>
-          <Text style={{ "text-color": dark }}>{post.slice(0, 32)}...</Text>
-          <Line
-            style={{ "line-color": dark }}
-            points={[
-              [0, 0],
-              [320, 0],
-            ]}
-          />
-        </>
-      ))}
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.title}>
+          UI Styling Test
+        </Text>
+      </View>
+      <Text style={styles.small}>Small Text</Text>
+      <Text style={styles.body}>Body Text</Text>
+      <Button style={styles.button}>This is a button</Button>
     </View>
   );
 }
+// const style = {
+//   view: {
+//       'transition-property': 'background-color, border-color, border-width',
+//       'transition-duration': '0.1s',
+//       'transition-timing-function': 'linear',
+//       'transition-delay': '0.2s',
+//   },
+//   pressedStyle: {
+//       'border-width': 6,
+//       'border-color': 'red',
+//       'background-color': 'red',
+
+//       'transition-property': 'background-color, border-color, border-width',
+//       'transition-duration': '0.5s',
+//       'transition-timing-function': 'linear',
+//       'transition-delay': '0',
+//   }
+// };
 
 Render.render(<App />);
